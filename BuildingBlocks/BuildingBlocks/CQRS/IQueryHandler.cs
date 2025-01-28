fc0aece13ee8832e707cd0634c776231315b593d
pratik1509 <gajjarpratikn@gmail.com>
2025-01-28 14:07:48 +0530

    Added IQueryHandler for CQRS

---
+++ BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,11 @@
+using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IQueryHandler<in TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
+    where TRequest : IQuery<TResponse>
+    where TResponse : notnull
+    {
+
+    }
+}
